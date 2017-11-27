3dd1d8eece00378e39dcf0ab9af33e7c579948a6
Christophe Coenraets <ccoenraets@gmail.com>
2017-11-27 09:48:02 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+## Pure Aloe Distributor App
+
+## Installation Instructions
+
+1. Install the Pure Aloe Salesforce application first. See instructions here: https://github.com/dreamforce17/purealoe
+
+1. In Setup > Users, create an integration user that you will use to connect to Salesforce from the Node.js app. Select **Salesforce** as the license type and **System Administrator** as the profile. Log in at least one with that user in a browser (https://test.salesforce.com), and select to not register your phone number.
+
+1. In Setup > Users > Permission Sets, assign your integration user to the purealoe permission set.
+
+1. Create a Connected App in a developer edition org or in your hub org (not in your scratch org)
+
+1. Clone this repository:
+    ```
+    git clone https://github.com/dreamforce17/purealoe-distributor
+    cd purealoe-distributor
+    ```
+
+1. Create a Heroku app:
+    ```
+    heroku create some_app_name
+    ```
+
+1. Create the Heroku config variables:
+    ```
+    heroku config:set SF_CLIENT_ID=your_connected_client_id
+    heroku config:set SF_CLIENT_SECRET=your_connected_client_secret
+    heroku config:set SF_USER_NAME=your_integration_user_name
+    heroku config:set SF_USER_PASSWORD=your_integration_user_password
+    heroku config:set SF_ENVIRONMENT=sandbox
+    ```
+
+    Set **SF_ENVIRONMENT** to **sandbox** if using a scratch org, or **production** if using a regular Developer Edition.
+
+1. Push the code to your Heroku app:
+    ```
+    git push heroku master
+    ```
+
+    or run the application locally:
+
+    ```
+    heroku run:local npm start
+    ```
---
+++ www/app.js
@@ -0,0 +1,157 @@
+var content = document.getElementById('content');
+var bundles;
+
+function renderBundleList() {
+    var html = '';
+    bundles.forEach(function(bundle) {
+        html = html + '<div class="row">' + renderBundle(bundle) + '</div>';
+    });
+    content.innerHTML = html;
+}
+
+function renderBundle(bundle, isAnimated) {
+    return `
+        <div class="col-sm-12">
+            <div class="panel panel-primary ${isAnimated?"animateIn":""}">
+                <div class="panel-heading">Bundle ID: ${bundle.bundleName}</div>
+                <div class="panel-body">
+                    <div class="col-md-12 col-lg-7">
+                        <table>
+                            <tr>
+                                <td class="panel-table-label">Description:</td><td>${bundle.bundleDescription}</td>
+                            </tr>
+                            <tr>
+                            <td class="panel-table-label">Items:</td><td>${bundle.qty}</td>
+                        </tr>
+                    </table>
+                    </div>   
+                    <div class="col-md-12 col-lg-5">
+                        <button class="btn btn-info" onclick="getBundleDetails('${bundle.bundleId}')" style="margin-bottom: 4px;">
+                            <span class="glyphicon glyphicon-zoom-in" aria-hidden="true"></span>
+                            View Details
+                        </button>
+                        <button class="btn btn-info" onclick="orderBundle('${bundle.bundleId}')" style="margin-bottom: 4px;">
+                            <span class="glyphicon glyphicon-ok" aria-hidden="true"></span>
+                            Order Bundle
+                        </button>
+                    </div>
+                    <div id="details-${bundle.bundleId}" class="col-md-12"></div>
+                </div>
+            </div>
+        </div>`;
+}
+
+// Render the merchandise list for a bundle
+function renderBundleDetails(bundle, items) {
+    var html = `
+        <table class="table">
+            <tr>
+                <th colspan="2">Product</th>
+                <th>MSRP</th>
+                <th>Qty</th>
+            </tr>`;
+    items.forEach(function(item) {
+        html = html + `
+            <tr>
+                <td><img src="${item.pictureURL}" style="height:50px"/></td>
+                <td>${item.productTitle}</td>
+                <td>$${item.price}</td>
+                <td>${item.qty}</td>
+            </tr>`
+    });
+    html = html + "</table>"    
+    var details = document.getElementById('details-' + bundle.bundleId);
+    details.innerHTML = html;
+}
+
+function deleteBundle(bundleId) {
+    for (var i = 0; i < bundles.length; i++) {
+        if (bundles[i].bundleId === bundleId) {
+            bundles.splice(i, 1);
+            break;
+        }
+    }
+}
+
+var socket = io.connect();
+
+socket.on('bundle_submitted', function (newBundle) {
+    // if the bundle is alresdy in the list: do nothing
+    var exists = false;
+    bundles.forEach((bundle) => {
+        if (bundle.bundleId == newBundle.bundleId) {
+            exists = true;
+        }
+    });
+    // if the bundle is not in the list: add it
+    if (!exists) {
+        bundles.push(newBundle);
+        var el = document.createElement("div");
+        el.className = "row";
+        el.innerHTML = renderBundle(newBundle, true);
+        content.insertBefore(el, content.firstChild);
+    }
+});
+
+socket.on('bundle_unsubmitted', function (data) {
+    deleteBundle(data.bundleId);
+    renderBundleList();
+});
+
+// Retrieve the existing list of bundles from Node server
+function getBundleList() {
+    var xhr = new XMLHttpRequest(),
+        method = 'GET',
+        url = '/bundles';
+
+    xhr.open(method, url, true);
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+        bundles = JSON.parse(xhr.responseText);
+        renderBundleList();
+    };
+    xhr.send();
+}
+
+// Retrieve the product list for a bundle from Node server
+function getBundleDetails(bundleId) {
+    var details = document.getElementById('details-' + bundleId);
+    if (details.innerHTML != '') {
+        details.innerHTML = '';
+        return;
+    }
+    var bundle;
+    for (var i=0; i<bundles.length; i++) {
+        if (bundles[i].bundleId === bundleId) {
+            bundle = bundles[i];
+            break;
+        }
+    };
+
+    var xhr = new XMLHttpRequest(),
+        method = 'GET',
+        url = '/bundles/' + bundleId;
+
+    xhr.open(method, url, true);
+    xhr.onload = function () {
+        var items = JSON.parse(xhr.responseText);
+        renderBundleDetails(bundle, items);
+    };
+    xhr.send();
+}
+
+// Post approve message to Node server
+function orderBundle(bundleId) {
+    var xhr = new XMLHttpRequest(),
+        method = 'POST',
+        url = '/approvals/' + bundleId;
+
+    xhr.open(method, url, true);
+    xhr.onload = function () {
+        deleteBundle(bundleId);
+        renderBundleList();
+    };
+    xhr.send();
+}
+
+getBundleList();
