b2be0a0ea6d69aa4f72a64588ee4eab02523e114
Chris Li <wfdlcl1227@126.com>
2020-04-20 14:22:37 +0800

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ let PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Express server listening on ${PORT}`));
 
 app.use(cors());
-app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/www/index.html');
-});
+app.use('/', express.static(__dirname + '/www'));
 
 io.on('connection', function (socket) {
     socket.emit('news', { hello: 'world' });
