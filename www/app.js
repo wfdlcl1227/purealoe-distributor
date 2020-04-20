ea8f2455a772086bf42095aff25a987404a932f3
Chris Li <wfdlcl1227@126.com>
2020-04-20 16:42:01 +0800

    Update app.js

--- www/app.js
+++ www/app.js
@@ -1,5 +1,38 @@
+var content = document.getElementById('content');
+
+
+
 var socket = io.connect();
 socket.on('news', function (data) {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
+	console.log(data);
+	socket.emit('myevent', { my: 'data' });
 });
+
+socket.on('myevent2', function (data) {
+	console.log(data);
+});
+
+function getBundleList() {
+    var xhr = new XMLHttpRequest(),
+        method = 'GET',
+        url = '/bundles';
+
+    xhr.open(method, url, true);
+    xhr.onload = function () {
+        console.log(xhr.responseText);
+        bundles = JSON.parse(xhr.responseText);
+        renderBundleList();
+    };
+    xhr.send();
+}
+
+
+function renderBundleList() {
+    var html = '';
+    bundles.forEach(function(bundle) {
+        html = html + '<div class="row">' + renderBundle(bundle) + '</div>';
+    });
+    content.innerHTML = html;
+}
+
+getBundleList();
