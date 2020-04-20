38a5753ba8e019f75d81f7b9e6e106d37638092f
Chris Li <wfdlcl1227@126.com>
2020-04-20 18:39:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -33,10 +33,10 @@ let getBundles = (req, res) => {
 };
 
 let getBundleDetails = (req, res) => {
-    let bundleId = req.params.bundleId;
-    let q = "SELECT Id, Merchandise__r.Name, Merchandise__r.Title__c, Merchandise__r.Price__c, Merchandise__r.Category__c, Merchandise__r.Picture_URL__c, Qty__c " +
-        "FROM Bundle_Item__c " +
-        "WHERE Bundle__c = '" + bundleId + "'";
+    let bundleId = "5002x000002Y8qa";
+    let q = "SELECT Id, Status, Reject_Reason__c, OwnerName__c " +
+        "FROM Case " +
+        "WHERE Id = '" + bundleId + "'";
     org.query({ query: q }, (err, resp) => {
         if (err) {
             console.log(err);
@@ -46,13 +46,9 @@ let getBundleDetails = (req, res) => {
             let prettyBundleItems = [];
             bundleItems.forEach(bundleItem => {
                 prettyBundleItems.push({
-                    productName: bundleItem.get("Merchandise__r").Name,
-                    productTitle: bundleItem.get("Merchandise__r").Title__c,
-                    price: bundleItem.get("Merchandise__r").Price__c,
-                    pictureURL: bundleItem.get("Merchandise__r").Picture_URL__c,
-                    bundleId: bundleItem.get("Id"),
-                    productId: bundleItem.get("Merchandise__r"),
-                    qty: bundleItem.get("Qty__c")
+                    Status: bundleItem.get("Status"),
+                    Reason: bundleItem.get("Reject_Reason__c"),
+                    name: bundleItem.get("OwnerName__c")
                 });
             });
             res.json(prettyBundleItems);
