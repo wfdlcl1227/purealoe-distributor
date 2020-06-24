a81c0c1fa6401e6942ea3c155eda3b5ac649287b
Chris Li <wfdlcl1227@126.com>
2020-06-24 11:08:30 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-'use strict'; 
-var express = require('express'); 
-var path = require('path'); 
-var port = process.env.PORT || 3000; 
+'use strict';  // 严格模式
+var express = require('express');  //引入模块 express
+var path = require('path'); // 引入路由模块
+var port = process.env.PORT || 5000;  //定义端口号
 var app = express();
+app.use(cors());
+app.use('/', express.static(__dirname + '/www'));    
 app.listen(port);
-console.log('lhapptool started on port ' + port); 
+console.log('lhapptool started on port ' + port);   //监听端口号
